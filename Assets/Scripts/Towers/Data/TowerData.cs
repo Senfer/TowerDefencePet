7f262474ee355f9826d44171b6e2b51798b1b5f9
RuslanPanteleev <panteleev.ruslan.v@gmail.com>
2023-10-08 19:08:01 +0300

    Added stub tower assets

---
+++ Assets/Scripts/Towers/Data/TowerData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TowerData.asset", menuName = "TowerDefensePet/TowerConfiguration", order = 1)]
+public class TowerData : ScriptableObject
+{
+    public int cost;
+
+    public int sell;
+}
